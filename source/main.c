2d164dde75c31dacae63bc711b59339d76b750bf
Quinn Leader <qlead001@ucr.edu>
2020-04-12 18:34:10 -0700

    Start part 3 code

--- source/main.c
+++ source/main.c
@@ -1,8 +1,8 @@
 /*	Author: Quinn Leader qlead001@ucr.edu
  *      Partner(s) Name: NA
  *	Lab Section: 026
- *	Assignment: Lab 4  Exercise 2
- *	Exercise Description: Counter
+ *	Assignment: Lab 4  Exercise 3
+ *	Exercise Description: Combination lock door
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -18,23 +18,39 @@ enum States {
     Release,
 } state;
 
-unsigned char count;
+enum Buttons {
+    X = 1,
+    Y = 2,
+    ENTER = 4,
+};
+
+unsigned char unlocked, index;
+const unsigned char comboLen = 2;
+const Buttons combo[] = {ENTER, Y};
 
 void Tick() {
     switch(state) { // Transitions
         case Start:
             state = Release;
-            count = 7;
+            unlocked = 0;
+            comboIndex = 0;
             break;
         case Press:
-            if (PINA&0x03) {
+            if (PINA&0x87) {
                 state = Release;
-                if ((PINA&0x03) == 0x01 && count < 9) count++;
-                else if ((PINA&0x03) == 0x02 && count > 0) count--;
+                if ((PINA&0x87) == 0x80) unlocked = 0;
+                else if ((PINA&0x07) == combo[comboIndex]) {
+                    if (++comboIndex >= comboLen) {
+                        unlocked = 1;
+                        comboIndex = 0;
+                    }
+                }
+                else if ((PINA&0x07) == ENTER) comboIndex = 1;
+                else comboIndex = 0;
             }
             break;
         case Release:
-            if (!(PINA&0x03)) state = Press;
+            if (!(PINA&0x87)) state = Press;
             break;
         default:
             state = Start;
@@ -48,12 +64,14 @@ void Tick() {
             break;
     } // State Actions
 
-    PORTC = count;
+    PORTB = unlocked;
+    PORTC = state;
 }
 
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
     DDRC = 0xFF; PORTC = 0x00;
     /* Insert your solution below */
     while (1) {
